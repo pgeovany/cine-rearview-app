99312d05c0f6e71b24545bdfec8c95a066eef7a2
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:08:14 -0300

    feat: add form component

---
+++ src/styles/auth/Form.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-items: left;
+  width: 80%;
+
+  label {
+    font-size: 18px;
+    color: #959596;
+  }
+`;
+
+export default Form;
