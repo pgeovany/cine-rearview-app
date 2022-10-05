34504ccc3b7142e89181fe452ab003db3d9fae56
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:09:22 -0300

    feat: implement sign up page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom';
+import Container from '../../styles/auth/Container';
+import Title from '../../styles/auth/Title';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import StyledLink from '../../styles/auth/StyledLink';
+
 export default function SignUp() {
-  return <>Sign-up page</>;
+  return (
+    <Container>
+      <Title>Cine Rearview</Title>
+      <SignUpForm />
+      <Link to="/">
+        <StyledLink>Already registered? Sign in!</StyledLink>
+      </Link>
+    </Container>
+  );
 }
