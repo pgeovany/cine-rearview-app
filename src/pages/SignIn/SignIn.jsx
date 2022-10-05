8f128cbb5826f7990704410357e1c0a5766bc6cc
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:14:56 -0300

    feat: implement sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom';
+import Container from '../../styles/auth/Container';
+import Title from '../../styles/auth/Title';
+import SignInForm from '../../components/SignInForm/SignInForm';
+import StyledLink from '../../styles/auth/StyledLink';
+
 export default function SignIn() {
-  return <>Sign-in page</>;
+  return (
+    <Container>
+      <Title>Cine Rearview</Title>
+      <SignInForm />
+      <Link to="/sign-up">
+        <StyledLink>{`Don't have an account yet? Sign up!`}</StyledLink>
+      </Link>
+    </Container>
+  );
 }
