80f1b57640f499fb9c2cba844a60052b69d70b51
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 14:00:55 -0300

    feat: implement basic route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
+import Search from './pages/Search/Search';
 
 function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
