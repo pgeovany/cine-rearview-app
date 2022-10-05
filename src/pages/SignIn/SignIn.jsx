87bad3ed7ed16694b55a7cb6c1e0abaecdbf3fad
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:46:29 -0300

    style: add logo to sign-in and sign-up screens

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,10 +3,13 @@ import Container from '../../styles/auth/Container';
 import Title from '../../styles/auth/Title';
 import SignInForm from '../../components/SignInForm/SignInForm';
 import StyledLink from '../../styles/auth/StyledLink';
+import Logo from '../../styles/Logo';
+import logo from '../../assets/images/logo.png';
 
 export default function SignIn() {
   return (
     <Container>
+      <Logo src={logo} />
       <Title>Cine Rearview</Title>
       <SignInForm />
       <Link to="/sign-up">
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,10 +3,13 @@ import Container from '../../styles/auth/Container';
 import Title from '../../styles/auth/Title';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import StyledLink from '../../styles/auth/StyledLink';
+import Logo from '../../styles/Logo';
+import logo from '../../assets/images/logo.png';
 
 export default function SignUp() {
   return (
     <Container>
+      <Logo src={logo} />
       <Title>Cine Rearview</Title>
       <SignUpForm />
       <Link to="/">
