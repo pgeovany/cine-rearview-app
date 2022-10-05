634a07db71ffc318a0d6e11b686f8052bf81a62e
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:08:06 -0300

    feat: add button component

---
+++ src/styles/auth/Button.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  height: 46px;
+  width: 100%;
+  border: none;
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  background-color: #e01f36;
+`;
+
+export default Button;
