68ab21e849bfb60dddcd1b670bf5eb941f870288
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:08:24 -0300

    feat: add generic container

---
+++ src/styles/auth/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export default Container;
