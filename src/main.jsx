7e2dc449bb713824c33721f3e2408a6d2182292a
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 15:05:01 -0300

    feat: implement global style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GlobalCSS from './styles/GlobalStyles';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalCSS />
     <App />
   </React.StrictMode>
 );
