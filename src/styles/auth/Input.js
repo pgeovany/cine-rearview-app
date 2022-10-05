390239e71181d504e9f3178cfc4fc269f538c1ce
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 17:08:37 -0300

    feat: add input component

---
+++ src/styles/auth/Input.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: inherit;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #959596;
+  color: white;
+  height: 40px;
+  width: 100%;
+  font-size: 16px;
+  padding-top: 18px;
+`;
+
+export default Input;
