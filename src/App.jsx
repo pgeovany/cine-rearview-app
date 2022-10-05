3b467122bf9f7c3bd84c42d7de90874103522bb0
Pablo Souza <pgeovany14@gmail.com>
2022-10-05 20:55:52 -0300

    feat: requires authentication for search route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContextProvider } from './contexts/UserContext';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import Search from './pages/Search/Search';
-import { UserContextProvider } from './contexts/UserContext';
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/search" element={<Search />} />
+          <Route
+            path="/search"
+            element={
+              <RequireAuth>
+                <Search />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </UserContextProvider>
     </BrowserRouter>
