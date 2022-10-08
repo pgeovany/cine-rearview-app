adffe8d9cbfb7d6b8dbc15e172bca5b5e024575d
Pablo Souza <pgeovany14@gmail.com>
2022-10-07 21:18:02 -0300

    feat: implement basic film details page

--- src/pages/FilmDetails/FilmDetails.jsx
+++ src/pages/FilmDetails/FilmDetails.jsx
@@ -1,6 +1,84 @@
-// import { useLocation } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import HorizontalBorder from '../../styles/HorizontalBorder';
+import {
+  Main,
+  Header,
+  Poster,
+  Title,
+  Overview,
+  ButtonContainer,
+  Button,
+} from './style';
+import { UserContext } from '../../contexts/UserContext';
+import api from '../../services/api';
 
 export default function FilmDetails() {
-  // const { state } = useLocation();
-  return <>FilmDetails page</>;
+  const { state } = useLocation();
+  const { userInfo } = useContext(UserContext);
+  const [film, setFilm] = useState(null);
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo?.token || ''}`,
+      },
+    };
+
+    async function fetchData() {
+      try {
+        const { data } = await api.get(
+          `/films/details/${state.originalId}`,
+          config
+        );
+        setFilm(data);
+      } catch (error) {
+        console.log(error?.response?.data);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  function genDetailsPage() {
+    if (film) {
+      return (
+        <>
+          <Header>
+            <Poster src={film.poster} />
+            <div>
+              <Title>{film.title}</Title>
+              <br />
+              <h2>
+                DIRECTED BY <br />
+                <span>
+                  {film?.directors?.map((director) => director).join(',')}
+                </span>
+              </h2>
+              <br />
+              <h2>Release date: {film.releaseDate}</h2>
+              <br />
+              <h2>Runtime: {film.runtime} min</h2>
+            </div>
+          </Header>
+          <br />
+          <HorizontalBorder />
+          <br />
+          <Overview>
+            OVERVIEW: <br />
+            {film.overview}
+          </Overview>
+          <br />
+          <HorizontalBorder />
+          <br />
+          <ButtonContainer>
+            <Button>Watched!</Button>
+          </ButtonContainer>
+        </>
+      );
+    }
+    return null;
+  }
+
+  return <Main>{genDetailsPage()}</Main>;
 }
---
+++ src/pages/FilmDetails/style.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import BaseButton from '../../styles/auth/Button';
+
+const Main = styled.div`
+  margin-top: 30px;
+  margin-bottom: 60px;
+  padding: 0 20px 0 20px;
+`;
+
+const Header = styled.div`
+  height: auto;
+  width: 100%;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  h2 {
+    color: white;
+    font-size: 18px;
+  }
+
+  span {
+    font-weight: bold;
+  }
+`;
+
+const Poster = styled.img`
+  width: 100px;
+  height: 150px;
+  float: right;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 26px;
+`;
+
+const Overview = styled.div`
+  color: white;
+  font-size: 18px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled(BaseButton)`
+  width: 40%;
+  position: absolute;
+  bottom: 20%;
+`;
+
+export { Main, Header, Poster, Title, Overview, ButtonContainer, Button };
